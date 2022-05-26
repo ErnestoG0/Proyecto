cb29a562b549da5e36d7e282674bbc94271695c7
Nintendoegg <66503979+Nintendoegg@users.noreply.github.com>
2022-05-26 16:39:57 -0600

    Subir documentos locales
    Proceso de subida de documentos

---
+++ Proyecto/SubidadeArchivo.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace ProcesoIncripcion
+{
+    partial class SubidadeArchivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPagoServicio = new System.Windows.Forms.Button();
+            this.lblPagorecibo = new System.Windows.Forms.Label();
+            this.SubidaArchivos = new System.Windows.Forms.OpenFileDialog();
+            this.txtPagoIncripcion = new System.Windows.Forms.TextBox();
+            this.btnActaNacimiento = new System.Windows.Forms.Button();
+            this.btnCurp = new System.Windows.Forms.Button();
+            this.lblActa = new System.Windows.Forms.Label();
+            this.lblCurp = new System.Windows.Forms.Label();
+            this.btnDomicilio = new System.Windows.Forms.Button();
+            this.lblDomicilio = new System.Windows.Forms.Label();
+            this.txtActa = new System.Windows.Forms.TextBox();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.txtCurp = new System.Windows.Forms.TextBox();
+            this.SubidadeActa = new System.Windows.Forms.OpenFileDialog();
+            this.btnSubirDocumentos = new System.Windows.Forms.Button();
+            this.SubidadeCurp = new System.Windows.Forms.OpenFileDialog();
+            this.lblCertificado = new System.Windows.Forms.Label();
+            this.btnCertificadoPrimaria = new System.Windows.Forms.Button();
+            this.txtCertificadoPrimaria = new System.Windows.Forms.TextBox();
+            this.DocumentoDomilicio = new System.Windows.Forms.OpenFileDialog();
+            this.CertificadoPrim = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnPagoServicio
+            // 
+            this.btnPagoServicio.Location = new System.Drawing.Point(52, 69);
+            this.btnPagoServicio.Name = "btnPagoServicio";
+            this.btnPagoServicio.Size = new System.Drawing.Size(170, 23);
+            this.btnPagoServicio.TabIndex = 0;
+            this.btnPagoServicio.Text = "Subir Archivo";
+            this.btnPagoServicio.UseVisualStyleBackColor = true;
+            this.btnPagoServicio.Click += new System.EventHandler(this.btnPagoServicio_Click);
+            // 
+            // lblPagorecibo
+            // 
+            this.lblPagorecibo.AutoSize = true;
+            this.lblPagorecibo.Location = new System.Drawing.Point(52, 51);
+            this.lblPagorecibo.Name = "lblPagorecibo";
+            this.lblPagorecibo.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblPagorecibo.Size = new System.Drawing.Size(73, 15);
+            this.lblPagorecibo.TabIndex = 1;
+            this.lblPagorecibo.Text = "Pago Recibo";
+            // 
+            // SubidaArchivos
+            // 
+            this.SubidaArchivos.FileName = "Subida de Archivos Pago";
+            // 
+            // txtPagoIncripcion
+            // 
+            this.txtPagoIncripcion.Location = new System.Drawing.Point(228, 69);
+            this.txtPagoIncripcion.Name = "txtPagoIncripcion";
+            this.txtPagoIncripcion.ReadOnly = true;
+            this.txtPagoIncripcion.Size = new System.Drawing.Size(73, 23);
+            this.txtPagoIncripcion.TabIndex = 2;
+            // 
+            // btnActaNacimiento
+            // 
+            this.btnActaNacimiento.Location = new System.Drawing.Point(52, 136);
+            this.btnActaNacimiento.Name = "btnActaNacimiento";
+            this.btnActaNacimiento.Size = new System.Drawing.Size(170, 23);
+            this.btnActaNacimiento.TabIndex = 3;
+            this.btnActaNacimiento.Text = "Subir Archivo";
+            this.btnActaNacimiento.UseVisualStyleBackColor = true;
+            this.btnActaNacimiento.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCurp
+            // 
+            this.btnCurp.Location = new System.Drawing.Point(52, 196);
+            this.btnCurp.Name = "btnCurp";
+            this.btnCurp.Size = new System.Drawing.Size(170, 23);
+            this.btnCurp.TabIndex = 4;
+            this.btnCurp.Text = "Subir Archivo";
+            this.btnCurp.UseVisualStyleBackColor = true;
+            this.btnCurp.Click += new System.EventHandler(this.btnCurp_Click);
+            // 
+            // lblActa
+            // 
+            this.lblActa.AutoSize = true;
+            this.lblActa.Location = new System.Drawing.Point(52, 118);
+            this.lblActa.Name = "lblActa";
+            this.lblActa.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblActa.Size = new System.Drawing.Size(112, 15);
+            this.lblActa.TabIndex = 5;
+            this.lblActa.Text = "Acta de Nacimiento";
+            // 
+            // lblCurp
+            // 
+            this.lblCurp.AutoSize = true;
+            this.lblCurp.Location = new System.Drawing.Point(52, 178);
+            this.lblCurp.Name = "lblCurp";
+            this.lblCurp.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblCurp.Size = new System.Drawing.Size(33, 15);
+            this.lblCurp.TabIndex = 6;
+            this.lblCurp.Text = "Curp";
+            // 
+            // btnDomicilio
+            // 
+            this.btnDomicilio.Location = new System.Drawing.Point(52, 252);
+            this.btnDomicilio.Name = "btnDomicilio";
+            this.btnDomicilio.Size = new System.Drawing.Size(170, 23);
+            this.btnDomicilio.TabIndex = 7;
+            this.btnDomicilio.Text = "Subir Archivo";
+            this.btnDomicilio.UseVisualStyleBackColor = true;
+            this.btnDomicilio.Click += new System.EventHandler(this.btnDomicilio_Click);
+            // 
+            // lblDomicilio
+            // 
+            this.lblDomicilio.AutoSize = true;
+            this.lblDomicilio.Location = new System.Drawing.Point(52, 234);
+            this.lblDomicilio.Name = "lblDomicilio";
+            this.lblDomicilio.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblDomicilio.Size = new System.Drawing.Size(124, 15);
+            this.lblDomicilio.TabIndex = 8;
+            this.lblDomicilio.Text = "Documento Domicilio";
+            // 
+            // txtActa
+            // 
+            this.txtActa.Location = new System.Drawing.Point(228, 136);
+            this.txtActa.Name = "txtActa";
+            this.txtActa.ReadOnly = true;
+            this.txtActa.Size = new System.Drawing.Size(73, 23);
+            this.txtActa.TabIndex = 9;
+            // 
+            // txtDomicilio
+            // 
+            this.txtDomicilio.Location = new System.Drawing.Point(228, 252);
+            this.txtDomicilio.Name = "txtDomicilio";
+            this.txtDomicilio.ReadOnly = true;
+            this.txtDomicilio.Size = new System.Drawing.Size(73, 23);
+            this.txtDomicilio.TabIndex = 10;
+            // 
+            // txtCurp
+            // 
+            this.txtCurp.Location = new System.Drawing.Point(228, 196);
+            this.txtCurp.Name = "txtCurp";
+            this.txtCurp.ReadOnly = true;
+            this.txtCurp.Size = new System.Drawing.Size(73, 23);
+            this.txtCurp.TabIndex = 11;
+            // 
+            // SubidadeActa
+            // 
+            this.SubidadeActa.FileName = "Subida de Acta Nacimiento";
+            // 
+            // btnSubirDocumentos
+            // 
+            this.btnSubirDocumentos.Location = new System.Drawing.Point(52, 354);
+            this.btnSubirDocumentos.Name = "btnSubirDocumentos";
+            this.btnSubirDocumentos.Size = new System.Drawing.Size(249, 55);
+            this.btnSubirDocumentos.TabIndex = 12;
+            this.btnSubirDocumentos.Text = "Subir Documentos";
+            this.btnSubirDocumentos.UseVisualStyleBackColor = true;
+            this.btnSubirDocumentos.Click += new System.EventHandler(this.btnSubirDocumentos_Click);
+            // 
+            // SubidadeCurp
+            // 
+            this.SubidadeCurp.FileName = "Subida de Curp";
+            // 
+            // lblCertificado
+            // 
+            this.lblCertificado.AutoSize = true;
+            this.lblCertificado.Location = new System.Drawing.Point(52, 299);
+            this.lblCertificado.Name = "lblCertificado";
+            this.lblCertificado.Size = new System.Drawing.Size(128, 15);
+            this.lblCertificado.TabIndex = 13;
+            this.lblCertificado.Text = "Certificado de Primaria";
+            // 
+            // btnCertificadoPrimaria
+            // 
+            this.btnCertificadoPrimaria.Location = new System.Drawing.Point(52, 317);
+            this.btnCertificadoPrimaria.Name = "btnCertificadoPrimaria";
+            this.btnCertificadoPrimaria.Size = new System.Drawing.Size(170, 23);
+            this.btnCertificadoPrimaria.TabIndex = 14;
+            this.btnCertificadoPrimaria.Text = "Subir Archivo";
+            this.btnCertificadoPrimaria.UseVisualStyleBackColor = true;
+            this.btnCertificadoPrimaria.Click += new System.EventHandler(this.btnCertificadoPrimaria_Click);
+            // 
+            // txtCertificadoPrimaria
+            // 
+            this.txtCertificadoPrimaria.Location = new System.Drawing.Point(228, 318);
+            this.txtCertificadoPrimaria.Name = "txtCertificadoPrimaria";
+            this.txtCertificadoPrimaria.ReadOnly = true;
+            this.txtCertificadoPrimaria.Size = new System.Drawing.Size(73, 23);
+            this.txtCertificadoPrimaria.TabIndex = 15;
+            // 
+            // DocumentoDomilicio
+            // 
+            this.DocumentoDomilicio.FileName = "DocumentoDomilicio";
+            // 
+            // CertificadoPrim
+            // 
+            this.CertificadoPrim.FileName = "Certificado";
+            // 
+            // SubidadeArchivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCertificadoPrimaria);
+            this.Controls.Add(this.btnCertificadoPrimaria);
+            this.Controls.Add(this.lblCertificado);
+            this.Controls.Add(this.btnSubirDocumentos);
+            this.Controls.Add(this.txtCurp);
+            this.Controls.Add(this.txtDomicilio);
+            this.Controls.Add(this.txtActa);
+            this.Controls.Add(this.lblDomicilio);
+            this.Controls.Add(this.btnDomicilio);
+            this.Controls.Add(this.lblCurp);
+            this.Controls.Add(this.lblActa);
+            this.Controls.Add(this.btnCurp);
+            this.Controls.Add(this.btnActaNacimiento);
+            this.Controls.Add(this.txtPagoIncripcion);
+            this.Controls.Add(this.lblPagorecibo);
+            this.Controls.Add(this.btnPagoServicio);
+            this.Name = "SubidadeArchivo";
+            this.Text = "Datos De Incripcion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnPagoServicio;
+        private Label lblPagorecibo;
+        private OpenFileDialog SubidaArchivos;
+        private TextBox txtPagoIncripcion;
+        private Button btnActaNacimiento;
+        private Button btnCurp;
+        private Label lblActa;
+        private Label lblCurp;
+        private Button btnDomicilio;
+        private Label lblDomicilio;
+        private TextBox txtActa;
+        private TextBox txtDomicilio;
+        private TextBox txtCurp;
+        private OpenFileDialog SubidadeActa;
+        private Button btnSubirDocumentos;
+        private OpenFileDialog SubidadeCurp;
+        private Label lblCertificado;
+        private Button btnCertificadoPrimaria;
+        private TextBox txtCertificadoPrimaria;
+        private OpenFileDialog DocumentoDomilicio;
+        private OpenFileDialog CertificadoPrim;
+    }
+}
---
+++ Proyecto/SubidadeArchivo.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Data;
+using System.Drawing.Imaging;
+using MySql.Data.MySqlClient;
+
+
+namespace ProcesoIncripcion
+{
+    public partial class SubidadeArchivo : Form
+    {
+        public SubidadeArchivo()
+        {
+            InitializeComponent();
+            
+        }
+        MySqlConnection mycon = new MySqlConnection("server=localhost;user id =root;password=EmilkarG97;database=admiescolar;sslmode=none");
+        MySqlDataAdapter da;
+        MySqlCommand cmd;
+        DataTable dt;
+        string sql, Alumid;
+        TextBox textBox ;
+        int resulta;
+        bool C1 = false, C2 = false, C3 = false, C4 = false, C5 = false;
+    
+        private void GuardarDatos()
+        {
+
+            try
+            {
+                
+                sql = "Insert into alumnos(AlumNombre,AlumApellidoPat,AlumApellidoMa,Grdo,Grpo,AlumCURP,AlumActNac,CertPrimaria) values('" +  DatosdeInscripcion.txtBoxNombre + "','" + DatosdeInscripcion.txtBoxApellidoPat + "','" + DatosdeInscripcion.txtBoxApellidoMat + "','" + DatosdeInscripcion.numBoxGrado + "','" + DatosdeInscripcion.numBoxGrupo + "','" + SubidadeCurp  + "','" + SubidadeActa + "','" + CertificadoPrim + "')";
+                mycon.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = mycon;
+                cmd.CommandText = sql;
+                resulta = cmd.ExecuteNonQuery();
+                if (resulta > 0)
+                {
+                    MessageBox.Show("Datos Guardados", "Guardado");
+                }
+                else
+                {
+                    MessageBox.Show("Fallo el Guardado", "Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mycon.Close();
+
+            }
+        }
+
+        private void btnPagoServicio_Click(object sender, EventArgs e)
+        {
+
+            
+            //configuracion  de algunos parametros del openFileDialog
+            // directorio inicial donde se abrira
+            SubidaArchivos.InitialDirectory = "C:\\";
+            // filtro de archivos.
+            SubidaArchivos.Filter = "Archivos de texto (*.pdf)|*.pdf";
+
+            // codigo para abrir el cuadro de dialogo
+            if (SubidaArchivos.ShowDialog() == DialogResult.OK)
+               
+            {               
+                try
+                {
+                    string str_SubidaArchivo = SubidaArchivos.FileName;
+                  
+                    txtPagoIncripcion.Text = str_SubidaArchivo;
+                    txtPagoIncripcion.BackColor = System.Drawing.Color.Green; 
+                    C1 = true;
+                }
+                catch (Exception)
+                {
+                    C2 = false;
+                    throw;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //configuracion  de algunos parametros del openFileDialog
+            // directorio inicial donde se abrira
+            SubidadeActa.InitialDirectory = "C:\\";
+            // filtro de archivos.
+            SubidadeActa.Filter = "Archivos de texto (*.pdf)|*.pdf";
+
+            // codigo para abrir el cuadro de dialogo
+            if (SubidadeActa.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string str_RutaArchivo = SubidadeActa.FileName;;
+                    txtActa.BackColor = System.Drawing.Color.Green;
+                    C2 = true;
+                }
+                catch (Exception)
+                {
+                    C2 = false;
+                    throw;
+                }
+            }
+        }
+
+        private void btnCertificadoPrimaria_Click(object sender, EventArgs e)
+        {
+            //configuracion  de algunos parametros del openFileDialog
+            // directorio inicial donde se abrira
+            CertificadoPrim.InitialDirectory = "C:\\";
+            // filtro de archivos.
+            CertificadoPrim.Filter = "Archivos de texto (*.pdf)|*.pdf";
+
+            // codigo para abrir el cuadro de dialogo
+            if (CertificadoPrim.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string str_RutaArchivo = CertificadoPrim.FileName;
+                    txtCertificadoPrimaria.BackColor = System.Drawing.Color.Green;
+                    C5 = true;
+                }
+                catch (Exception)
+                {
+                    C5 = false;
+                    throw;
+                }
+            }
+        }
+
+        private void btnCurp_Click(object sender, EventArgs e)
+        {
+            //configuracion  de algunos parametros del openFileDialog
+            // directorio inicial donde se abrira
+            SubidadeCurp.InitialDirectory = "C:\\";
+            // filtro de archivos.
+            SubidadeCurp.Filter = "Archivos de texto (*.pdf)|*.pdf";
+
+            // codigo para abrir el cuadro de dialogo
+            if (SubidadeCurp.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string str_RutaArchivo = SubidadeCurp.FileName;
+                    txtCurp.BackColor = System.Drawing.Color.Green;
+                    C3 = true;
+                }
+                catch (Exception)
+                {
+                    C2 = false;
+                    throw;
+                }
+            }
+        }
+
+        private void btnDomicilio_Click(object sender, EventArgs e)
+        {
+            //configuracion  de algunos parametros del openFileDialog
+            // directorio inicial donde se abrira
+            DocumentoDomilicio.InitialDirectory = "C:\\";
+            // filtro de archivos.
+            DocumentoDomilicio.Filter = "Archivos de texto (*.pdf)|*.pdf";
+
+            // codigo para abrir el cuadro de dialogo
+            if (DocumentoDomilicio.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+                    string str_RutaArchivo = DocumentoDomilicio.FileName;                   
+                    //txtPagoIncripcion.Text = str_RutaArchivo;
+                    txtDomicilio.BackColor = System.Drawing.Color.Green;
+                    C4= true; 
+                }
+                catch (Exception)
+                {
+                    C2 = false;
+                    throw;
+                }
+            }
+        }
+
+        private void btnSubirDocumentos_Click(object sender, EventArgs e)
+        {
+            
+            if (C1 && C2 && C3 && C4 && C5 == true)
+            {
+                GuardarDatos();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Error Faltan Archivo a subir");
+            }
+        }
+    }
+}
