1891f069175c9e3cb1f41ce48911425a79172ec6
Ernesto Garcia <trollear425@gmail.com>
2022-05-27 16:25:52 -0600

    Debug

--- Proyecto/conexion.cs
+++ Proyecto/conexion.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using System.Data.SqlClient;
 using System.Data;
 
