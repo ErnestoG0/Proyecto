68a4db1d66ffc0944c011974f54cc1d11120e5ef
Ernesto Garcia <ernesto.g0@outlook.com>
2022-11-04 11:03:09 -0700

    Datos de conexion

--- Pago/conexion.php
+++ Pago/conexion.php
@@ -1,24 +1,15 @@
 <?php
-
-class Cconexion{
-    
-    static function ConexionDB(){
-        #$server = 'sql.freedb.tech';
-        #$username = 'freedb_AdProTienAd';
-        #$password = 'hwGsZ7PHU9egUT#';
-        #$database = 'freedb_ProTienAd';
-        $server = 'localhost';
-        $username = 'root';
-        $password = '';
-        $database = '';
+    $server = 'database-1.can0dvlvifnp.us-west-2.rds.amazonaws.com';
+    $username = 'ProyAdmin123';
+    $password = 'Pa$$w0rd123';
+    $database = 'Tienda';
         
-        try {
-            $conn = new PDO("mysql:host=$server;dbname=$database",$username,$password);
-            echo("Se conecto Correctamente a la base de datos");
-        } catch(PDOExeption $exp){
-            echo("No se Pudo conectar a la base de Datos :( , error: $exp");
-        }
-        return $conn;
+    try {
+        $conn = new PDO("mysql:host=$server;dbname=$database",$username,$password);
+        echo("Se conecto Correctamente a la base de datos");
+    } catch(PDOExeption $exp){
+        echo("No se Pudo conectar a la base de Datos :( , error: $exp");
     }
-}
+    return $conn;
+
 ?>
